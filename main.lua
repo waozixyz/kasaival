0962d93ed807aa2605de19ccd5a7decaa65422c2
waotzi <waotzi@proton.me>
2018-11-10 13:08:15 +0000

    draw rectangle

--- main.lua
+++ main.lua
@@ -1,13 +1,22 @@
 local lg=love.graphics
 
-function love.load()
+local Ground = {}
+local Player = {}
 
+
+function love.load()
+  
 end
 
-function love.update(dt)
 
+
+function love.update(dt)
+  
 end
 
 function love.draw()
+  W, H = lg.getDimensions()
 
+  lg.setColor(0.4, 0.2, 0.1 ) 
+  lg.rectangle('fill', 0, H * 0.4 , W, H * 0.6 )
 end
