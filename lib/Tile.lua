4d07efc4b3c9de8787a4dffd08a02633b322f5d0
waotzi <waotzi@proton.me>
2018-11-18 23:18:50 +0000

    add Tile (forgot to add it earlier)

---
+++ lib/Tile.lua
@@ -0,0 +1,29 @@
+require 'class'
+
+local lg=love.graphics
+local random=love.math.random
+
+local Tile = class(function(self,shape,color)
+  self.shape=shape
+  self.x,self.y=shape[1],shape[3]
+  self.color=color or {.2,.2,.6}
+end)
+--[[
+function Tile:update()
+  local red = self.color[1]
+  if red > .7 then
+    red = red - .04
+  elseif red > .4 then
+    red = red - .02
+  elseif red > .3 then
+    red = red - random(0,1)*.01
+  end
+end--]]
+
+function Tile:draw()
+  lg.setColor(self.color)
+  lg.polygon('fill', self.shape)
+end
+
+return Tile
+ 
