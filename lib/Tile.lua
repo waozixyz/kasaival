1094b7c26c491728d126aaefd1759f471454cb45
waotzi <waotzi@proton.me>
2018-11-20 20:15:21 +0000

    improve tile

--- lib/Tile.lua
+++ lib/Tile.lua
@@ -15,19 +15,18 @@ local Tile = class(function(self,shape,r,g,b,w,h)
   
   self.w = w or 32
   self.h = h or 32
+  self.burn = false
 end)
 
 function Tile:getHitbox()
   return {self.x, self.x + self.w, self.y + self.h*.8, self.y + self.h}
 end
 
-function Tile:burn()
-  self.r = self.r + 0.1
-end
-
 function Tile:collide(o)
-  if o.element == 'fire' and self.burnable then
-    self:burn()
+  if o.element == 'fire' then
+    if self.burnable then
+      self.burn = true
+    end
   end
 end
 
@@ -37,29 +36,35 @@ function Tile:update()
   local el  = self.element
 
   if r > .6 then
-    r = r - .03
-    g = g - .04
-  elseif r > .4 then
     r = r - .02
     g = g - .03
-  elseif r > .2 then
+  elseif r > .4 then
     r = r - .01
     g = g - .02
+  elseif r > .2 then
+    r = r - .005
+    g = g - .01
   end
- 
-  if g > .6 then
-  elseif g > .4 then
-    g = g + .03
+  
+  if self.burn then
+    r = r + .02
   else
-    g = g + .02
+    if g > .6 then
+    elseif g > .4 then
+      g = g + .005
+    else
+      g = g + .01
+    end
   end
- 
-  if g > .4 then
+
+  if g > .1 then
     self.burnable = true
   else
     self.burnable = false
-  end 
+  end
+
   self.r,self.g,self.b = r,g,b
+  self.burn = false
 end
 
 function Tile:draw()
