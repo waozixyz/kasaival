9efbfb6da4d817bf7fc38929a1b4ae4cd6777b8b
waotzi <waotzi@proton.me>
2018-12-02 14:25:16 +0000

    improve shuriken

--- lib/Shuriken.lua
+++ lib/Shuriken.lua
@@ -11,23 +11,24 @@ local Shuriken=class(function(self, x, y, dx, dy)
   self.hp = 100
   self.atk = 18
   self.sx=1
-  self.size=.3
+  self.size=.2
   self.hpsize=true
 end)
 
-function Shuriken:update()
-  self.x = self.x + self.dx
-  self.y = self.y + self.dy
-  self.hp = self.hp - 1
+function Shuriken:update(dt)
+  local sc=self.sx*self.size*8 
+  self.x = self.x + self.dx*sc
+  self.y = self.y + self.dy*sc
+  self.hp = self.hp - 2
   
 end
 
 
 function Shuriken:draw()
-  lg.setColor(1,1,0)
+  lg.setColor(1,1,1)
   local w,h=self.img:getDimensions()
   local sc=self.sx*self.size 
-  lg.draw(self.img, self.x, self.y, 0, sc, sc, w*.5,h*.5)
+  lg.draw(self.img, self.x, self.y, 0, sc, sc, nil,h)
 end
 
 return Shuriken
