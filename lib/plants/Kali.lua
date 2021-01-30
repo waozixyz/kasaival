e0c6ca9519360ae70f71710b2d4d60206c5e8bcd
Speedkun <Speedkun>
2021-01-30 17:53:58 +0000

    more realistic dogs

--- lib/plants/Kali.lua
+++ lib/plants/Kali.lua
@@ -10,7 +10,8 @@ local function init(self)
     self.windx=1
     self.windy=1
     self.windstark=1
-    dog = love.graphics.newImage( "runningdog.png")
+    dog = love.graphics.newImage( "solodog.png")
+
     
     self.test=0
     return self
@@ -18,17 +19,16 @@ end
 
 local function draw(self)
     --verschiedene herumwandernde Etwas
+    love.graphics.draw(dog, 1000+self.spawnmodifikator*70-self.dampf^2, 600+math.sin(self.spawnmodifikator)*self.spawnmodifikator*self.spawnmodifikator/self.dampf)
     
-    love.graphics.draw(dog, 1000+self.spawnmodifikator*70-self.dampf^2, 600+math.sin(self.spawnmodifikator)*self.spawnmodifikator*self.spawnmodifikator/self.dampf, 50)
-    
-    love.graphics.draw(dog, 1050+math.sin(self.spawnmodifikator)*70-self.spawnmodifikator, 650+math.sin(self.spawnmodifikator)*self.spawnmodifikator*self.spawnmodifikator/self.dampf+self.spawnmodifikator, 40)
-    love.graphics.draw(dog, 1100+(math.cos(self.spawnmodifikator)+1)*self.dampf*self.dampf, 500+math.sin(self.spawnmodifikator)*self.spawnmodifikator, 20)
+    love.graphics.draw(dog, 1050+math.sin(self.spawnmodifikator)*70-self.spawnmodifikator, 650+math.sin(self.spawnmodifikator)*self.spawnmodifikator*self.spawnmodifikator/self.dampf+self.spawnmodifikator)
+    love.graphics.draw(dog, 1100+(math.cos(self.spawnmodifikator)+1)*self.dampf*self.dampf, 500+math.sin(self.spawnmodifikator)*self.spawnmodifikator)
    
-    love.graphics.draw(dog, 1150+(math.sin(self.spawnmodifikator)+1)*self.dampf*self.dampf-10*self.dampf, 500+10*self.spawnmodifikator+math.sin(self.spawnmodifikator)*self.spawnmodifikator, 20)
+    love.graphics.draw(dog, 1150+(math.sin(self.spawnmodifikator)+1)*self.dampf*self.dampf-10*self.dampf, 500+10*self.spawnmodifikator+math.sin(self.spawnmodifikator)*self.spawnmodifikator)
     
-    love.graphics.draw(dog, 1400+self.spawnmodifikator*70-self.dampf^2, 680+math.sin(self.spawnmodifikator+self.spawnmodifikator)*self.spawnmodifikator*self.spawnmodifikator/self.dampf, 50-self.spawnmodifikator)
+    love.graphics.draw(dog, 1400+self.spawnmodifikator*70-self.dampf^2, 680+math.sin(self.spawnmodifikator+self.spawnmodifikator)*self.spawnmodifikator*self.spawnmodifikator/self.dampf)
    
-    love.graphics.draw(dog, 800+self.spawnmodifikator*90-self.dampf^2, 800+math.sin(self.dampf)*self.spawnmodifikator*self.spawnmodifikator/self.dampf, 50)
+    love.graphics.draw(dog, 800+self.spawnmodifikator*90-self.dampf^2, 800+math.sin(self.dampf)*self.spawnmodifikator*self.spawnmodifikator/self.dampf)
     --windpfeil darstellung
     love.graphics.setColor(0, 0, 0)
     love.graphics.line( 200, 200, 200+self.windx*self.windstark,200+ self.windy*self.windstark)
