1a047abf6638fb9f57179421bb17029014a404ae
Speedkun <Speedkun>
2021-02-01 14:13:00 +0000

    somedogpinke#2l

--- lib/ps/pinkelx.lua
+++ lib/ps/pinkelx.lua
@@ -19,7 +19,6 @@ image1:setFilter("linear", "linear")
 
 local ps = LG.newParticleSystem(image1, 36)
 ps:setColors(0.75389224290848, 0.84848487377167, 0.016069788485765, 1, 0.60044723749161, 0.69318181276321, 0.013128443621099, 0.62878787517548, 0.83092284202576, 0.96212118864059, 0, 0.75378787517548, 0.90909093618393, 1, 0, 0.69318181276321)
-ps:setDirection(2.2236430644989)
 ps:setEmissionArea("none", 0, 0, 0, false)
 ps:setEmissionRate(19.100435256958)
 ps:setEmitterLifetime(10.942975997925)
@@ -36,8 +35,8 @@ ps:setSizeVariation(0)
 ps:setSpeed(90, 100)
 ps:setSpin(0, 0)
 ps:setSpinVariation(0)
-ps:setSpread(0.46115121245384)
-ps:setTangentialAcceleration(0, 0)
+ps:setSpread(0.11528780311346)
+ps:setTangentialAcceleration(2.4262187480927, 0.87343871593475)
 
 
 return ps
