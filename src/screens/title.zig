8411f00820bce63e2dfe61f533cda547cce4d298
wolfi <wolfi@disroot.org>
2022-03-15 00:18:15 +0100

    add title screen

--- src/screens/title.zig
+++ src/screens/title.zig
@@ -11,9 +11,15 @@ pub const TitleScreen = struct{
     pub fn load(_: *TitleScreen) void {
     }
     pub fn update(_: *TitleScreen) void {
+        if (ray.IsMouseButtonPressed(ray.MOUSE_LEFT_BUTTON) or ray.GetKeyPressed() > 0) {
+            print("hi", .{});
+        }
     }
     pub fn draw(self: *TitleScreen) void {
         ray.DrawTexture(self.texture, 0, 0, ray.WHITE);
+        ray.DrawText("KASAIVAL", 480, 160, 200, ray.MAROON);
+        ray.DrawText("an out of control flame trying to survive", 350, 640, 60, ray.MAROON);
+        ray.DrawText("touch anywhere to start burning", 480, 1000, 60, ray.BEIGE);
     }
     pub fn unload(self: *TitleScreen) void {
         ray.UnloadTexture(self.texture);
