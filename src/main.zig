496119a9379d36e955e6e7cc229fbd5fce8c7de2
wolfi <wolfi@disroot.org>
2022-03-14 23:54:58 +0100

    add scaling logic

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,14 @@ const ray = @cImport({
     @cInclude("raylib.h");
 });
 
+const print = std.debug.print;
+
 const title_screen = @import("screens/title.zig");
+
+const gameWidth : f16 = 1920;
+const gameHeight: f16 = 1080;
+
+fn min(a: f16, b: f16) f16 { if (a < b) { return a; } else { return b; } }
 
 pub fn main() void {
     // Initialization
@@ -13,6 +20,13 @@ pub fn main() void {
     ray.SetConfigFlags(ray.FLAG_WINDOW_RESIZABLE);
     ray.InitWindow(screenWidth, screenHeight, "Kasaival");
     ray.SetTargetFPS(60);
+    
+    // Render texture initialization, used to hold the rendering result so we can easily resize it
+    var target = ray.LoadRenderTexture(gameWidth, gameHeight);
+    ray.SetTextureFilter(target.texture, ray.TEXTURE_FILTER_BILINEAR);
+
+    // init audio device
+    ray.InitAudioDevice();
 
     var screen = title_screen.new();
     screen.load();
@@ -22,6 +36,8 @@ pub fn main() void {
     while (!ray.WindowShouldClose()) {
         // Update
         //----------------------------------------------------------------------------------
+        const scale = min(@intToFloat(f16, ray.GetScreenWidth()) / gameWidth, @intToFloat(f16, ray.GetScreenHeight()) / gameHeight);
+
         if (ray.IsKeyPressed(ray.KEY_F)) ray.ToggleFullscreen();
         screen.update();
         //----------------------------------------------------------------------------------
@@ -29,8 +45,16 @@ pub fn main() void {
         // Draw
         //----------------------------------------------------------------------------------
         ray.BeginDrawing();
-        ray.ClearBackground(ray.RAYWHITE);
+        ray.ClearBackground(ray.BLACK);
+        ray.BeginTextureMode(target);
         screen.draw();
+        ray.EndTextureMode();
+
+        // Draw RenderTexture2D to window, properly scaled
+        const texture_rect = ray.Rectangle{.x = 0, .y = 0, .width = @intToFloat(f16, target.texture.width), .height = @intToFloat(f16, -target.texture.height)};
+        const screen_rect = ray.Rectangle{.x = (@intToFloat(f16, ray.GetScreenWidth()) - gameWidth * scale) * 0.5, .y = (@intToFloat(f16, ray.GetScreenHeight()) - gameHeight * scale) * 0.5, .width = gameWidth * scale, .height = gameHeight * scale};
+        ray.DrawTexturePro(target.texture, texture_rect, screen_rect, ray.Vector2{.x = 0, .y = 0}, 0.0, ray.WHITE);
+       
         ray.EndDrawing();
         //----------------------------------------------------------------------------------
     }
